2a4ac3c729086bb420a549f3807f6d4dc6e996f0
Anton Hvornum <anton.feeds@gmail.com>
2018-03-14 22:28:24 +0100

    Some fixes

--- broken.js
+++ broken.js
@@ -5,17 +5,17 @@
 	We'll do this by getting the last loaded script (us)
 	and check the `.src` tag, split it, remove the end and voila.
 */
-var loadedScripts = document.getElementsByTagName('script');
-var path = loadedScripts[loadedScripts.length-1].src.split('?')[0];
-var __base__ = path.split('/').slice(0, -1).join('/')+'/';
+const loadedScripts = document.getElementsByTagName('script');
+const path = loadedScripts[loadedScripts.length-1].src.split('?')[0];
+const __base__ = path.split('/').slice(0, -1).join('/')+'/';
 
 /* Includes */
 // TODO: The correct way of doing it (Chrome 70+):
 //    import {varReference|*} from './brokenJS/hacks.js';
 //
 function ES5_import(url, callback=null) {
-	var head = document.getElementsByTagName('head')[0];
-	var script = document.createElement('script');
+	const head = document.getElementsByTagName('head')[0];
+	const script = document.createElement('script');
 	script.type = 'text/javascript';
 	script.src = url;
 
@@ -28,17 +28,26 @@ function ES5_import(url, callback=null) {
 	document.currentScript.parentNode.insertBefore(script, document.currentScript);
 }
 
-ES5_import(__base__+'./subresources/hacks.js');
-ES5_import(__base__+'./subresources/constants.js');
-ES5_import(__base__+'./subresources/helpers.js');
+ES5_import(__base__+'subresources/hacks.js');
+ES5_import(__base__+'subresources/constants.js');
+ES5_import(__base__+'subresources/helpers.js');
 
 var timers = {};
+var frames = {};
+
+const key = {
+	ENTER : 13
+};
+
+var div_keyHooks = {};
 
 class brokenObj {
 	constructor (id, content, on_load=null) {
 		this.id = id;
 		this.obj = null;
 		this.content = content;
+		this.parent = window;
+		frames[this.id] = this;
 		if(!on_load)
 			on_load = this.update;
 
@@ -56,6 +65,20 @@ class brokenObj {
 		}
 	}
 
+	remove() {
+		if(this.obj)
+			this.obj.remove();
+		delete(frames[this.id]);
+		// Something else.
+	}
+
+	click(f) {
+		if (this.obj)
+			this.obj.onclick = f;
+		else
+			this.onclick = f;
+	}
+
 	update() {
 		console.log('Object ' + this.id + ' now exists');
 		this.obj.id = this.id;
@@ -65,18 +88,25 @@ class brokenObj {
 	}
 
 	append_to(what) {
+		this.parent = what;
 		what.obj.appendChild(this.obj);
 	}
 }
 
 class div extends brokenObj {
 
-	constructor(id, x=0, y=0, width=AUTO, height=AUTO, on_load=null) {
-		super(id, on_load=on_load);
+	constructor(id, x=0, y=0, width=AUTO, height=AUTO, content=null, on_load=null) {
+		super(id, content, on_load);
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+
+		this.key_events = {};
+	}
+
+	clear() {
+		this.obj.innerHTML = '';
 	}
 
 	create() {
@@ -85,13 +115,50 @@ class div extends brokenObj {
 		if(this.x || this.y)
 			this.obj.style.position = 'absolute';
 		this.obj.style.left = this.x; 
-		this.obj.style.top = this.x;
+		this.obj.style.top = this.y;
+		this.obj.onkeypress = this._onKeyPress;
 
 		//var tmp = this.update;
 		//var tmpUpdate = tmp.bind(this);
 		//tmpUpdate();
 		//this.update();
 		this.update();
+		return this;
+	}
+
+	_onKeyPress(event) {
+		var symbol = event.keyCode || event.which;
+
+		if ((symbol in div_keyHooks) && (this.id in div_keyHooks[symbol])) {
+			const ctx = div_keyHooks[symbol][this.id]['context'];
+			div_keyHooks[symbol][this.id]['func'].call(ctx, this, event);
+		}
+	}
+
+	/** Function to _register_ key-press events.
+	*
+	* @param {Integer} key - defines the symbol (keyboard value) pressed to hook in to.
+	* @param {Function} func - the function() code called when a trigger is met.
+	*/
+	on_keypress(key, func) {
+		if(!(key in div_keyHooks))
+			div_keyHooks[key] = {}
+		
+		div_keyHooks[key][this.id] = {'context': this, 'func' : func};
+	}
+
+	absolute() {
+		this.obj.style.position = 'absolute';
+	}
+
+	add_element(obj) {
+		if(obj) {
+			if(typeof obj === str)
+				this.obj.innerHTML = this.obj.innerHTML + obj;
+			else
+				this.obj.appendChild(obj.obj);
+			this.parent = obj;
+		}
 	}
 
 	update(styledata={}) {
@@ -115,13 +182,15 @@ class div extends brokenObj {
 		if(this.x || this.y)
 			this.obj.style.position = 'absolute';
 		this.obj.style.left = this.x; 
-		this.obj.style.top = this.x;
+		this.obj.style.top = this.y;
 
 		if(this.content) {
-			if(typeof this.content === str)
-				this.obj.innerHTML = this.content;
-			else
-				this.obj.appendChild(this.content);
+			this.add_element.call(this, this.content);
+//			if(typeof this.content === str)
+//				this.obj.innerHTML = this.content;
+//			else
+//				this.obj.appendChild(this.content);
+			this.content = null;
 		}
 	}
 }
--- subresources/helpers.js
+++ subresources/helpers.js
@@ -40,11 +40,11 @@ function getObj(id, parent=null) {
 */
 function populate(context, what, With, func, poll_interval=50) {
 	var UID = With+'_'+gen_UID();
-	set_timer(UID, function() {
+	setTimer(UID, function() {
 		var o = getObj(With);
 		if(o) {
 			let varName = varReference(what);
-			console.log('Populating ' + varName + ' with ' + With);
+//			console.log('Populating ' + varName + ' with ' + With);
 			context[varName] = o;
 
 			func.call(context);
@@ -57,12 +57,24 @@ function populate(context, what, With, func, poll_interval=50) {
 			//}
 
 			//window[what] = o;
-			clear_timer(UID);
+			clearTimer(UID);
 		}
 	}, poll_interval);
 }
 
-function clear_timer(name) {
+function destroy(obj) {
+	// Try to remove on the class handle (first by string ref, then obj.id ref),
+	// if it's missing, we'll call remove on the DOM.
+
+	if (typeof obj === str && obj in frames)
+		frames[obj].remove();
+	else if(obj.id !== undefined && obj.id in frames)
+		frames[obj.id].remove();
+	else
+		obj.remove();
+}
+
+function clearTimer(name) {
 	if(timers[name] !== undefined) {
 		window.clearInterval(timers[name]);
 		return true;
@@ -70,6 +82,72 @@ function clear_timer(name) {
 	return false;
 }
 
-function set_timer(name, func, time=10) {
+function setTimer(name, func, time=10) {
 	timers[name] = setInterval(func, time);
+}
+
+/*
+> This belongs in a module, probably. But as of now,
+  It's so tiny that it can live here.
+*/
+class socket {
+	constructor(host, port=80, url='/') {
+		this.host = host;
+		this.port = port;
+		this.url = url;
+		this.connection = null;
+
+		this.connect();
+	}
+
+	connect(host=null, port=null) {
+		if(!this.host && host)
+			this.host = host;
+		if(!this.port && port)
+			this.port = port;
+
+		if(this.connection && this.connection.readyState != 3)
+			this.connection.close()
+
+		this.connection = new WebSocket('ws://'+this.host+':'+this.port+'/'+this.url);
+		this.connection.onerror = this.onerror;
+		this.connection.onmessage = this.onmessage;
+		this.connection.onopen = this.onopen;
+	}
+
+	send(data) {
+		if(typeof data !== str) {
+			data = JSON.stringify(data);
+		}
+
+		// If connection is broken, reconnect and send
+		if(this.connection && this.connection.readyState == 3) {
+			this.connect();
+			this.connection.onopen = function(event) {
+				this.connection.send(data);
+				this.connection.onopen = this.onopen;
+			}
+		// If not broken, send instantly.
+		} else {
+			this.connection.send(data);
+		}
+	}
+
+	connected() {
+		if(this.connection && this.connection.readyState == 2)
+			return true;
+		return false;
+	}
+
+	onopen(event) {
+		// Empty placeholder
+	}
+
+	onerror(event) {
+		// Empty placeholder
+	}
+
+	onmessage(event) {
+		// Empty placeholder
+	}
 }
